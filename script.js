c6f207e90727ad09694cb43b3bb563ac7fa0b431
Shirley Zhang <shirley.zhang@hotmail.com.au>
2017-08-22 01:30:58 +1000

    Shows table of movies when you hit look

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+init();
+
+function init() {
+	app = {
+		movies: []
+	};
+
+
+	cons = {
+		movieFields: ["name", "runtime", "genreOne", "genreTwo"]
+	}
+
+}
+
 function getRandomMovie() {
 	var min = document.getElementById("minNum").value;
 	var max = document.getElementById("maxNum").value;
@@ -16,8 +30,42 @@ function randomNum(min, max) {
 }
 
 // 
+function drawList() {
+	var table = document.getElementById('movieTable');
+	
+	let tr = document.createElement('tr');
+	for (let j = 0; j < cons.movieFields.length; j++) {
+		let field = cons.movieFields[j];
+
+		let td = document.createElement('td');
+		let txt = document.createTextNode(field);
+
+		td.appendChild(txt);
+		tr.appendChild(td);
+	}
+	table.appendChild(tr);
+
+	for (let i = 0; i < app.movies.length; i++) {
+		let movie = app.movies[i];
+
+		let tr = document.createElement('tr');
+		for (let j = 0; j < cons.movieFields.length; j++) {
+			let field = cons.movieFields[j];
+
+			let td = document.createElement('td');
+			let txt = document.createTextNode(movie[field]);
+
+			td.appendChild(txt);
+			tr.appendChild(td);
+		}
+
+		table.appendChild(tr);
+	}
+
+}
+
 function parseCsvMovieList() {
-	var csv = document.getElementById("movieList").value;
+	var csv = document.getElementById("csvInput").value;
 	console.log(csv);
 	// Testing only
 	if (csv == "") {
@@ -25,16 +73,16 @@ function parseCsvMovieList() {
 	}
 
 	// sweet sweet string manip action
-	var movieFields = ["name", "runtime", "genreOne", "genreTwo"];
+	var movieFields = cons.movieFields;
 	var splitCsv = csv.split("\n");
 	console.log(splitCsv);
 
 	var parsedObj = [];
-	for (var i = 0; i < splitCsv.length; i++) {
-		var movie = {};
-		var movieInfo = splitCsv[i].split(',');
+	for (let i = 0; i < splitCsv.length; i++) {
+		let movie = {};
+		let movieInfo = splitCsv[i].split(',');
 
-		for (var j = 0; j < movieFields.length; j++) {
+		for (let j = 0; j < movieFields.length; j++) {
 			if (movieInfo[j] != undefined) {
 				movieInfo[j] = movieInfo[j].trim();
 				movie[movieFields[j]] = movieInfo[j];
@@ -46,7 +94,12 @@ function parseCsvMovieList() {
 		}
 	}
 
+	app.movies = parsedObj;
+
+	drawList();
+
 	console.log("== Movies from csv");
-	console.log(parsedObj);
+	console.log(app.movies);
+
 	return parsedObj;
 }
