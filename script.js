57ddedb26c20ba3b10b540d11b22abce369ad39d
Shirley Zhang <shirley.zhang@hotmail.com.au>
2017-08-21 11:49:07 +1000

    Parses csvlist of movies, added input tags to html page

--- script.js
+++ script.js
@@ -14,3 +14,39 @@ function randomNum(min, max) {
 	var result = Math.floor((Math.random() * max) + min);
 	return result;
 }
+
+// 
+function parseCsvMovieList() {
+	var csv = document.getElementById("movieList").value;
+	console.log(csv);
+	// Testing only
+	if (csv == "") {
+		csv = "A Monster in Paris, 90, Fantasy, Adventure\nTestMovie, 100, Genre 1\nTestOtherMovie, 120, Adventure, Action";
+	}
+
+	// sweet sweet string manip action
+	var movieFields = ["name", "runtime", "genreOne", "genreTwo"];
+	var splitCsv = csv.split("\n");
+	console.log(splitCsv);
+
+	var parsedObj = [];
+	for (var i = 0; i < splitCsv.length; i++) {
+		var movie = {};
+		var movieInfo = splitCsv[i].split(',');
+
+		for (var j = 0; j < movieFields.length; j++) {
+			if (movieInfo[j] != undefined) {
+				movieInfo[j] = movieInfo[j].trim();
+				movie[movieFields[j]] = movieInfo[j];
+			}
+		}
+
+		if (movie["name"] != undefined) {
+			parsedObj.push(movie);
+		}
+	}
+
+	console.log("== Movies from csv");
+	console.log(parsedObj);
+	return parsedObj;
+}
